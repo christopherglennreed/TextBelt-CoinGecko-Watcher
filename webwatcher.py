2d8546046be5bb4e236ffd751d359c25c680f97c
ChrisgReed <christopherglennreed@gmail.com>
2023-03-10 09:57:02 -0500

    Update webwatcher.py

--- webwatcher.py
+++ webwatcher.py
@@ -8,6 +8,7 @@ parser.add_argument('-p', help="Phone Number Text Belt Api will reach out to" ,d
 parser.add_argument('-a', help="api from TextBelt" ,dest="key", type=str, required=True)
 parser.add_argument('-u',help="url to watch" ,dest="url", type=str, required=False)
 parser.add_argument('-t',help="sleep time" ,dest="time", type=int, required=True)
+parser.add_argument('-k',help="Text to id or watch until gone" ,dest="txt", type=int, required=True)
 args=parser.parse_args()
 
 Headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
@@ -17,7 +18,7 @@ while True:
   data = requests.get(url=args.url, headers=Headers)
   data = data.text
   txtmsg = "looks good to buy!" + (args.url) 
-  if not "This product is excluded from all promotional" in data : 
+  if not "args.txt" in data : 
      print("Time to buy as the shoes are no longer prevented from discount")
      resp = requests.post('https://textbelt.com/text', {
        'phone': args.phone,
