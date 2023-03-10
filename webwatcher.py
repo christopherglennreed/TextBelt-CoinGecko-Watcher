7dcf82b58a19cf61aeb8b88c35174391301666c6
ChrisgReed <christopherglennreed@gmail.com>
2023-03-10 10:03:43 -0500

    Update webwatcher.py

--- webwatcher.py
+++ webwatcher.py
@@ -8,7 +8,7 @@ parser.add_argument('-p', help="Phone Number Text Belt Api will reach out to" ,d
 parser.add_argument('-a', help="api from TextBelt" ,dest="key", type=str, required=True)
 parser.add_argument('-u',help="url to watch" ,dest="url", type=str, required=False)
 parser.add_argument('-t',help="sleep time" ,dest="time", type=int, required=True)
-parser.add_argument('-k',help="Text to id or watch until gone" ,dest="txt", type=string, required=True)
+parser.add_argument('-k',help="Text to id or watch until gone" ,dest="txt", type=str, required=True)
 args=parser.parse_args()
 
 Headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
